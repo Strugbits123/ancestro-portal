3b274dabfd21f3e1dd8452a61b92f1949cb0a8a9
awaiszubair39 <awais@strugbitsglobal.com>
2025-11-21 22:58:33 +0500

    SignatureVariable --removed

--- app/api/file-upload/route.js
+++ app/api/file-upload/route.js
@@ -6,7 +6,6 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  signatureVersion: "v4" 
 });
 
 export async function POST(req) {
